1de8f24772f928c37b0a07362dc0382cd65b0b2a
Jihooyim <jihooyim@gmail.com>
2020-12-26 11:26:01 +0900

    Initial Commit

---
+++ Memong/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memong
+//
+//  Created by punky on 2020/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memong/MemongApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemongApp.swift
+//  Memong
+//
+//  Created by punky on 2020/12/26.
+//
+
+import SwiftUI
+
+@main
+struct MemongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
